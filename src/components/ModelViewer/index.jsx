9e6e1b943fe02130979aa1fff3724de5d499b236
rafelis1997 <rafaelheros80@gmail.com>
2022-10-05 08:36:47 -0300

    enable quick-look

--- src/components/ModelViewer/index.jsx
+++ src/components/ModelViewer/index.jsx
@@ -9,7 +9,7 @@ export function ModelViewer() {
         camera-controls
         autoplay
         ar 
-        ar-modes="webxr scene-viewer"
+        ar-modes="webxr scene-viewer quick-look"
       >
         {/* <button 
           className='absolute bottom-14 left-1/2 transform -translate-x-1/2 font-bold text-xl text-slate-50 py-4 px-8 rounded-md bg-[#6e5dcf]'
