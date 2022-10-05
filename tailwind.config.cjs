1c556419dfbd1d686352a8389f135cdba53020b2
rafelis1997 <rafaelheros80@gmail.com>
2022-10-05 09:49:17 -0300

    tailwind config break point

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -8,23 +8,8 @@ module.exports = {
       sans: ['Roboto','sans-serif']
     },
     screens: {
-      'mobile': '300px',
-      // => @media (min-width: 300px) { ... }
-
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
-
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
-
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
-
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
-
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
+      mobile: '300px',
+      ...defaultTheme.screens,
     }
   },
   plugins: [],
