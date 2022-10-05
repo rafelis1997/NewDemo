7b497df8985bd551472c5ec8dba3b39b7d4d438d
rafelis1997 <rafaelheros80@gmail.com>
2022-10-05 08:18:51 -0300

    return to scene-viewer

--- src/components/ModelViewer/index.jsx
+++ src/components/ModelViewer/index.jsx
@@ -7,17 +7,14 @@ export function ModelViewer() {
         alt="A model of me"
         shadow-intensity="1"
         camera-controls
-        auto-play
-        animation-name="waving"
-        ar
-        xr-environment
+        autoplay
+        ar 
+        ar-modes="webxr scene-viewer"
       >
         <button 
           className='absolute bottom-14 left-1/2 transform -translate-x-1/2 font-bold text-xl text-slate-50 py-4 px-8 rounded-md bg-[#6e5dcf]'
           slot='ar-button'
-        >
-          Activate AR
-        </button>
+        >Activate AR</button>
       </model-viewer>
   )
 } 
